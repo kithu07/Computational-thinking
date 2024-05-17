22557c62ef8a486fe7981d045825f4952f4bfcde
Keerthana <dskithu07@gmail.com>
2024-05-17 18:04:38 +0530

    Modified the Odometer() and added a readme

---
+++ bootcamp/odometer/README.md
@@ -0,0 +1,13 @@
+# ODOMETER PROBLEM
+
+Conditions to be satisfied: 
+1. Should'nt have 0
+2. Strictly ascending
+3. Fixed size
+
+Implement functions to compute:
+1. Next reading, provided the current reading
+2. Previous reading, provided the next reading
+3. Reading after n steps, given the current reading
+4. Reading before n steps, given the current reading
+5. Steps between 2 given readings.
--- bootcamp/odometer/odometer.py
+++ bootcamp/odometer/odometer.py
@@ -6,15 +6,23 @@ def possible_readings(size: int):
     return [distance for distance in range(lower_limit, higher_limit) if strictly_ascending(distance) and '0' not in str(distance)]
 
 def next_reading(current_reading: int, readings: list):
+    if readings.index(current_reading) == len(readings) - 1:
+        return readings[0]
     return readings[readings.index(current_reading) + 1]
 
 def prev_reading(current_reading: int, readings: list):
+    if readings.index(current_reading) == 0:
+        return readings[-1]
     return readings[readings.index(current_reading) - 1]
 
 def reading_after_n_steps(current_reading: int, steps: int, readings: list):
+    if readings.index(current_reading) > len(readings) - 1 - steps:
+        return readings[steps - (len(readings) - readings.index(current_reading))]
     return readings[readings.index(current_reading) + steps]
 
 def reading_before_n_steps(current_reading: int, steps: int, readings: list):
+    if readings.index(current_reading) < steps:
+        return readings[-1 * (steps - readings.index(current_reading))]
     return readings[readings.index(current_reading) - steps]
 
 def step_difference(reading1: int, reading2: int, readings: list):
@@ -22,7 +30,9 @@ def step_difference(reading1: int, reading2: int, readings: list):
 
 readings = possible_readings(3)
 print(next_reading(234, readings))
+print(next_reading(789, readings))
 print(prev_reading(234, readings))
-print(reading_after_n_steps(234, 2, readings))
-print(reading_before_n_steps(234, 2, readings))
-print(step_difference(234, 567, readings))
+print(prev_reading(123, readings))
+print(reading_after_n_steps(789, 2, readings))
+print(reading_before_n_steps(123, 2, readings))
+print(step_difference(234, 236, readings))
